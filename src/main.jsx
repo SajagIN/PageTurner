2b8f323090d1dc6f0b6ecffe05e6024fedba1921
SajagIN <sajag1403@gmail.com>
2025-06-29 23:22:22 +0530

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.jsx';
+// import { ThemeProvider } from './ThemeContext.jsx';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Router>
+//     <ThemeProvider>
+//       <App />
+//     </ThemeProvider>
+//   </Router>
+// );
+
+
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // Remove <React.StrictMode> temporarily for debugging stability
-  // The Router and ThemeProvider are now managed inside App.jsx
-  <App />
+  <Router>
+    <App />
+  </Router>
 );
