5e4b6b3c1621a97207495ff10e2209ee262c4430
SajagIN <sajag1403@gmail.com>
2025-06-29 23:14:12 +0530

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import { ThemeProvider } from './ThemeContext.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </Router>
+  // TEMPORARILY REMOVE React.StrictMode for debugging this specific issue
+  <App />
 );
