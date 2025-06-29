8dd53f03aefe5f71f66125b8eccca81cf44a71f6
SajagIN <sajag1403@gmail.com>
2025-06-29 23:22:38 +0530

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,6 @@ import App from './App.jsx';
 //   </Router>
 // );
 
-
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
